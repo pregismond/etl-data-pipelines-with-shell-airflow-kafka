cc08da5b9235648d6647ad59dde9c644f44f1965
Pravin Regismond <139565322+pregismond@users.noreply.github.com>
2024-04-17 14:16:46 -0400

    Initial commit

---
+++ bash/airflow/dags/ETL_toll_data.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""
+SCRIPT: ETL_toll_data.py
+AUTHOR: Pravin Regismond
+DATE: 2024-04-17
+DESCRIPTION: Final Assignment (Part 1) - Creating ETL Data Pipelines using
+             Bash with Apache Airflow
+
+"""
+
+# Import libraries
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+# Task 1.1 - Define DAG arguments
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(0),
+    'email': ['airflow@example.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Task 1.2 - Define the DAG
+dag = DAG(
+    dag_id='ETL_toll_data',
+    schedule_interval=timedelta(days=1),
+    default_args=default_args,
+    description='Apache Airflow Final Assignment',
+)
+
+# Task 1.9 - Create a task
+extract_transform_load = BashOperator(
+    task_id="extract_transform_load",
+    bash_command="/home/project/airflow/dags/Extract_Transform_data.sh ",
+    dag=dag,
+)
+
+# task pipeline
+extract_transform_load
---
+++ bash/airflow/dags/Extract_Transform_data.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#/*****************************************************************
+# SCRIPT: Extract_Transform_data.sh
+# AUTHOR: Pravin Regismond
+# DATE: 2024-04-11
+# DESCRIPTION: Final Assignment (Part 1) - Creating ETL Data
+#              Pipelines using Bash with Apache Airflow 
+#
+#******************************************************************
+# AUDIT TRAIL START                               INIT  DATE
+# ----------------------------------------------  ----- -----------
+# 1. Initial version                              PR    2024-04-11
+#
+# AUDIT TRAIL END
+#******************************************************************/
+
+# Unzip the data
+echo "Unzipping the data..."
+tar -xzf /home/project/airflow/dags/tolldata.tgz -C /home/project/airflow/dags
+
+# Extract data from CSV file
+echo "Extracting data from the CSV file..."
+cut -d "," -f1-4 /home/project/airflow/dags/vehicle-data.csv > /home/project/airflow/dags/csv_data.csv
+
+# Extract data from TSV file
+echo "Extracting data from the TSV file..."
+cut -f5-7 /home/project/airflow/dags/tollplaza-data.tsv | tr "\t" "," > /home/project/airflow/dags/tsv_data.csv
+
+# Extract data from fixed-width file
+echo "Extracting data from the fixed-width file..."
+cut -c 59- /home/project/airflow/dags/payment-data.txt | tr " " "," > /home/project/airflow/dags/fixed_width_data.csv
+
+# Consolidate data from previous tasks
+echo "Consolidating data from previous tasks..."
+paste -d "," /home/project/airflow/dags/csv_data.csv \
+    /home/project/airflow/dags/tsv_data.csv \
+    /home/project/airflow/dags/fixed_width_data.csv \
+    > /home/project/airflow/dags/extracted_data.csv
+
+# Transform and load the data
+echo "Transforming and loading the data..."
+sed 's/[^,]*/\U&/4' /home/project/airflow/dags/extracted_data.csv > /home/project/airflow/dags/transformed_data.csv
+
+echo "Data processing completed!"
