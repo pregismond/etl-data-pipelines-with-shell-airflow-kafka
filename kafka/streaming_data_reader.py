fd92ccce7e3db04984f85f214166c0fdc32c5788
Pravin Regismond <139565322+pregismond@users.noreply.github.com>
2024-04-19 12:08:16 -0400

    Initial commit

---
+++ kafka/streaming_data_reader.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+"""
+SCRIPT: streaming_data_reader.py
+AUTHOR: IBM
+DATE: 2022-09-21
+DESCRIPTION: Streaming data consumer
+
+AUDIT TRAIL START                               INIT  DATE
+----------------------------------------------  ----- -----------
+1. Initial version                              IBM   2022-09-21
+2. Updated constant variables                   PR    2024-04-11
+
+AUDIT TRAIL END
+"""
+import os
+from datetime import datetime
+from kafka import KafkaConsumer
+import mysql.connector
+
+TOPIC='toll'
+DATABASE = 'tolldata'
+USERNAME = 'root'
+PASSWORD = os.environ.get("MYSQL_PASSWORD")
+
+print("Connecting to the database")
+try:
+    connection = mysql.connector.connect(host='localhost', database=DATABASE, user=USERNAME, password=PASSWORD)
+except Exception:
+    print("Could not connect to database. Please check credentials")
+else:
+    print("Connected to database")
+cursor = connection.cursor()
+
+print("Connecting to Kafka")
+consumer = KafkaConsumer(TOPIC)
+print("Connected to Kafka")
+print(f"Reading messages from the topic {TOPIC}")
+for msg in consumer:
+
+    # Extract information from kafka
+
+    message = msg.value.decode("utf-8")
+
+    # Transform the date format to suit the database schema
+    (timestamp, vehcile_id, vehicle_type, plaza_id) = message.split(",")
+
+    dateobj = datetime.strptime(timestamp, '%a %b %d %H:%M:%S %Y')
+    timestamp = dateobj.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Loading data into the database table
+
+    sql = "insert into livetolldata values(%s,%s,%s,%s)"
+    result = cursor.execute(sql, (timestamp, vehcile_id, vehicle_type, plaza_id))
+    print(f"A {vehicle_type} was inserted into the database")
+    connection.commit()
+connection.close()
---
+++ mysql/livetolldata_health.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#/*****************************************************************
+# SCRIPT: livetolldata_health.sh
+# AUTHOR: Pravin Regismond
+# DATE: 2024-04-11
+# DESCRIPTION: Health check of the streaming data pipeline
+#
+#******************************************************************
+# AUDIT TRAIL START                               INIT  DATE
+# ----------------------------------------------  ----- -----------
+# 1. Initial version                              PR    2024-04-11
+#
+# AUDIT TRAIL END
+#******************************************************************/
+
+# Connect to MySQL and perform query
+mysql --host=127.0.0.1 --port=3306 --user=root --password=${MYSQL_PASSWORD} << EOF
+-- List the top 10 rows in the table livetolldata
+system echo 'Listing the top 10 rows in the livetolldata table';
+use tolldata;
+select * from livetolldata limit 10;
+exit
+EOF
