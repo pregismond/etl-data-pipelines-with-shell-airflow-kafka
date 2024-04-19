b407955addf1d4a36a6455171779024abf1d082b
Pravin Regismond <139565322+pregismond@users.noreply.github.com>
2024-04-19 15:13:27 -0400

    Initial commit

---
+++ kafka/kafka_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#/*****************************************************************
+# SCRIPT: kafka_install.sh
+# AUTHOR: Pravin Regismond
+# DATE: 2024-04-11
+# DESCRIPTION: Exercise 1 - Prepare the lab environment
+#              - Download Kafka
+#              - Extract Kafka
+#
+#******************************************************************
+# AUDIT TRAIL START                               INIT  DATE
+# ----------------------------------------------  ----- -----------
+# 1. Initial version                              PR    2024-04-11
+#
+# AUDIT TRAIL END
+#******************************************************************/
+
+# Download Kafka
+wget https://archive.apache.org/dist/kafka/2.8.0/kafka_2.12-2.8.0.tgz
+
+# Extract Kafka
+tar -xzf kafka_2.12-2.8.0.tgz
